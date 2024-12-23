ee11de3ea71cd44b8b2005998bfa7910dc191889
lyule <lyule@bc.edu>
2024-12-23 14:04:02 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import OperationMenu from './components/operation_menu';
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./App.css"
+import OperationMenu from "./components/operation_menu"
+import ItemMenu from "./components/item_menu"
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <h1>Welcome to the teaching tools collection</h1>
-        <Routes>
-          <Route path="/operation-menu" element={<OperationMenu />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	const mockData = require("./mockData.json")
+	console.log(mockData)
+	return (
+		<>
+			<h1>Welcome to the teaching tools collection</h1>
+			<OperationMenu />
+			<ItemMenu data={mockData} />
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/item_menu.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const ItemMenu = ({ data }) => {
+	return (
+		<>
+			<h1>Item Menu</h1>
+			<ul>
+				{data.map((item, index) => (
+					<li key={index}>
+						<h2>{item.app_name}</h2>
+						<p>{item.app_description}</p>
+						<a
+							href={item.app_uri}
+							target="_blank"
+							rel="noopener noreferrer">
+							{item.app_uri}
+						</a>
+					</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+export default ItemMenu
