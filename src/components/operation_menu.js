9721893348eaac1480c236d1ad15f027fc761247
lyule <lyule@bc.edu>
2024-12-29 20:58:14 -0500

    adding 1/3 of operation menu

--- src/components/operation_menu.js
+++ src/components/operation_menu.js
@@ -3,19 +3,45 @@ import "../styles/OperationMenu.css"
 const OperationMenu = () => {
 	const [status, setStatus] = useState("None Selected")
 
-	const handleStart = () => setStatus("Add a new app")
-	const handleStop = () => setStatus("Delete Selected")
-	const handleReset = () => setStatus("Select All")
+	const handleAddMenu = () => setStatus("Adding A New App")
+	const handleDeleteMenu = () => setStatus("Deleting Selected")
+	const handleSelectAllMenu = () => setStatus("Selecting All")
+
+	const handleAdd = (e) => {
+		e.preventDefault()
+		const formData = new FormData(e.target)
+		const formProps = Object.fromEntries(formData.entries())
+		alert(
+			`App Name: ${formProps.appName}, App Notes: ${formProps.appNotes}, App URI: ${formProps.appURI}`
+		)
+	}
 
 	return (
 		<>
 			<h1>Operation Menu</h1>
 			<div className="button-group">
-				<button onClick={handleStart}>Add a new app</button>
-				<button onClick={handleStop}>Delete Selected</button>
-				<button onClick={handleReset}>Select All</button>
+				<button onClick={handleAddMenu}>Add a new app</button>
+				<button onClick={handleDeleteMenu}>Delete Selected</button>
+				<button onClick={handleSelectAllMenu}>Select All</button>
 			</div>
 			<p>Status: {status}</p>
+			{status === "Adding A New App" && (
+				<form onSubmit={handleAdd}>
+					<label>
+						App Name:
+						<input type="text" name="appName" />
+					</label>
+					<label>
+						App Notes:
+						<input type="text" name="appNotes" />
+					</label>
+					<label>
+						App URI:
+						<input type="text" name="appURI" />
+					</label>
+					<button type="submit">Add</button>
+				</form>
+			)}
 		</>
 	)
 }
